3ed13365556c053828dc7e48be782d1098311981
Somashekar17 <49157790+somashekar17@users.noreply.github.com>
2022-07-20 20:05:56 +0530

    Add files via upload

---
+++ CG/brenham's.c
@@ -0,0 +1,100 @@
+Implement Brenham’s line drawing algorithm for all types of slope
+
+
+#include<GL/glut.h>
+#include<stdio.h>
+int x1,y1,x2,y2;
+void minit()
+{
+glClear(GL_COLOR_BUFFER_BIT);
+glClearColor(0.0,0.0,0.0,1.0);
+glMatrixMode(GL_PROJECTION);
+gluOrtho2D(0,500,0,500);
+}
+void dp(int x,int y)
+{
+glBegin(GL_POINTS);
+glVertex2i(x,y);
+glEnd();
+}
+void dl(int x1,int x2,int y1,int y2)
+{
+int x,y,cx,cy,dx,dy,c1,c2,i,e;
+dx=x2-x1;
+dy=y2-y1;
+if(dx<0) dx=-dx;
+if(dy<0) dy=-dy;
+cx=1;
+if(x2<x1)
+cx=-1;
+cy=1;
+if(y2<y1)
+cy=-1;
+x=x1;
+y=y1;
+if(dx>dy)
+{
+printf("%d\t%d\n",x,y);
+dp(x,y);
+e=2*dy-dx;
+c1=2*(dy-dx);
+c2=2*dy;
+for(i=0;i<dx;i++)
+{
+if(e>=0)
+{
+y+=cy;
+e+=c1;
+}
+else
+e+=c2;
+x+=cx;
+printf("%d\t%d\n",x,y);
+dp(x,y);
+}
+}
+else
+{
+printf("%d\t%d\n",x,y);
+dp(x,y);
+e=2*dy-dx;
+c1=2*(dx-dy);
+c2=2*dx;
+for(i=0;i<dy;i++)
+{
+if(e>=0)
+{
+x+=cx;
+e+=c1;
+}
+else
+e+=c2;
+y+=cy;
+printf("%d\t%d\n",x,y);
+dp(x,y);
+}
+}
+}
+void mdisplay()
+{
+dl(x1,x2,y1,y2);
+glFlush();
+}
+int main(int argc,char **argv)
+{
+glutInit(&argc,argv);
+printf("enter 1st point\n");
+scanf(“%d%d”, &x1, &y1);
+printf("enter 2nd point\n");
+scanf(“%d%d”, &x2, &y2);
+Computer Graphics Laboratory with Mini Project
+Dept. of CSE, JSSATE Page 14
+printf("\n\n\n\n");
+glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+glutInitWindowSize(1000,1000);
+glutInitWindowPosition(0,0);
+glutCreateWindow("bresenhams");
+minit();
+glutDisplayFunc(mdisplay);
+glutMainLoop();
+}
--- CG/rotation triangle.c
+++ CG/rotation triangle.c
@@ -1,9 +1,7 @@
 #include<stdio.h>
 #include<math.h>
 #include<GL/glut.h>
-GLfloat t[3][3]={{10.0,30.0,20.0},
-				 {20.0,20.0,40.0},
-				 {1.0,1.0,1.0}};
+GLfloat t[3][3]={{10.0,30.0,20.0},{20.0,20.0,40.0},{1.0,1.0,1.0}};
 GLfloat rotatemat[3][3]={{0},{0},{0}};
 GLfloat result[3][3]={{0},{0},{0}};
 GLfloat xr=10.0;
@@ -25,10 +23,11 @@ void rotate_about_origin()
 {
 GLfloat m,n;
 m=xr*(1-cos(theta))+yr*sin(theta);
-n=yr*(1-cos(theta))+xr*sin(theta);
+n=yr*(1-cos(theta))-xr*sin(theta);
 rotatemat[0][0]=cos(theta);
 rotatemat[0][1]=-sin(theta);
 rotatemat[0][2]=0;
+
 rotatemat[1][0]=sin(theta);
 rotatemat[1][1]=cos(theta);
 rotatemat[1][2]=0;
@@ -60,13 +59,14 @@ glBegin(GL_LINE_LOOP);
 glColor3f(1.0,0.0,0.0);
 glVertex2f(t[0][0],t[1][0]);
 glColor3f(0.0,1.0,0.0);
+
 glVertex2f(t[0][1],t[1][1]);
 glColor3f(0.0,0.0,1.0);
 glVertex2f(t[0][2],t[1][2]);
 glEnd();
 glFlush();
 }
-void draw_rotated_triangle()
+void draw_roatated_triangle()
 {
 glLineWidth(10);
 glBegin(GL_LINE_LOOP);
@@ -86,7 +86,15 @@ if(ch==1)
 {
 draw_triangle();
 rotate_about_origin();
-draw_rotated_triangle();
+draw_roatated_triangle();
+glFlush();
+}
+if(ch==2)
+{
+
+draw_triangle();
+rotate_about_fixed_point();
+draw_roatated_triangle();
 glFlush();
 }
 }
@@ -97,12 +105,12 @@ glMatrixMode(GL_PROJECTION);
 glLoadIdentity();
 gluOrtho2D(-50.0,50.0,-50.0,50.0);
 }
-int main(int argc,char **argv)
+int main(int argc,char**argv)
 {
-printf("***rotation***\n1.rotation about origin\n2.rotation about fixed polygon(xr,yr)\n");
-printf("enter choice:\n");
+printf("***rotation**\n1.rotation about origin\n2.rotation about an fixed ponit(xr,yr)\n" );
+printf("enter the choice\n");
 scanf("%d",&ch);
-printf("enter the rotation angle:\n");
+printf("enter the rotation angle\n");
 scanf("%f",&theta);
 theta=theta*(3.14/180);
 glutInit(&argc,argv);
